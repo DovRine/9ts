5208801feecdb445666c3066702767ecbbb77892
Dov Rine <dov.rine@gmail.com>
2023-04-08 15:59:50 -0700

    fix(getApiUrl): port is not defined

--- frontend/src/lib/getApiUrl.js
+++ frontend/src/lib/getApiUrl.js
@@ -1,6 +1,6 @@
 function getApiUrl() {
-    const host = window.location.hostname;
-    const port = process.env.REACT_APP_BACKEND_PORT
+    const host = window.location.hostname ?? 'club9ts.com';
+    const port = process.env.REACT_APP_BACKEND_PORT ?? 5000
     const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http'
     return `${protocol}://${host}:${port}`
 }
